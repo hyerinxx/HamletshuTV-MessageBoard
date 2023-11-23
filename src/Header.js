6eb3de770bcbb98179e74fb256d8bfd9813768d3
Hyerin Sung <hyerinx@naver.com>
2023-11-23 20:14:19 +0900

    비전공자가 알려주는 React, Node.js, MongoDB로 게시판 만들기 - 3강 React 화면 구성

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Button, Image } from "react-bootstrap";
+import axios from "axios";
+import $ from "jquery";
+import {} from "jquery.cookie";
+
+axios.defaults.withCredentials = true;
+const headers = { withCredentials: true };
+
+class Header extends Component {
+
+}
